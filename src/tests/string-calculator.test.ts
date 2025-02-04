b74b8fd059d0a4a09cf65b6a053694e8521c27a6
ramvignesh-b <ramvignesh@incubyte.co>
2025-02-05 00:26:07 +0530

    test: handles multiple longer-length separators

--- src/tests/string-calculator.test.ts
+++ src/tests/string-calculator.test.ts
@@ -1,55 +1,54 @@
 import Calculator from '../string-calculator';
-import {describe, expect, it} from "vitest";
+import {beforeEach, describe, expect, it} from "vitest";
 
 describe('String Calculator', () => {
+    let calculator: Calculator;
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
     it("should return 0 for an empty string", () => {
-        const calculator = new Calculator();
         expect(calculator.add("")).toBe(0);
     });
 
     it("should return the number for a single number", () => {
-        const calculator = new Calculator();
         expect(calculator.add("1")).toBe(1);
     });
 
     it("should return the sum for two numbers separated by a comma", () => {
-        const calculator = new Calculator();
         expect(calculator.add("1,2")).toBe(3);
     });
 
     it("should return the sum for multiple numbers separated by a comma", () => {
-        const calculator = new Calculator();
         expect(calculator.add("1,2,3")).toBe(6);
     });
 
     it('should handle newlines as a separator', () => {
-        const calculator = new Calculator();
         expect(calculator.add("1\n2,3")).toBe(6);
     });
 
     it('should handle custom delimiters', () => {
-        const calculator = new Calculator();
         expect(calculator.add("//;\n1;2")).toBe(3);
     });
 
     it('should throw error on negative numbers', () => {
-        const calculator = new Calculator();
         expect(() => calculator.add("1,-2")).toThrowError("negatives not allowed: -2");
     });
 
     it('should ignore numbers greater than 1000', () => {
-        const calculator = new Calculator();
         expect(calculator.add("2,1001,3")).toBe(5);
     });
 
     it('should handle arbitrary length of separators', () => {
-        const calculator = new Calculator();
         expect(calculator.add("//[***]\n1***2***3")).toBe(6);
     });
 
     it('should handle multiple single length separators', () => {
-        const calculator = new Calculator();
         expect(calculator.add("//[*][%]\n1*2%3")).toBe(6);
+    });
+
+    it('should handle multiple longer-length separators', () => {
+        expect(calculator.add("//[foo][bar]\n1foo2bar3")).toBe(6);
     });
 });
 
